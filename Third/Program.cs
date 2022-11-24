b5505390da7cdc285b3bdbff2651b268ff77611a
Dmitriy <Wonderful_days@mail.ru>
2022-11-24 23:07:23 +0300

    Complete third task

---
+++ Third/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Tape number ");
+string A = Console.ReadLine();
+int Number = Convert.ToInt32(A);
+
+if (Number % 2 == 0) Console.WriteLine($"{Number} is an even number");
+
+else Console.WriteLine($"{Number} is an odd number");
