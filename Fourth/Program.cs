ba33eb4aafe73167419a99d1facbed6209f96917
Dmitriy <Wonderful_days@mail.ru>
2022-11-24 23:31:51 +0300

    Complete fourth task

---
+++ Fourth/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int N = 2;
+
+Console.Write("Tape number ");
+string A = Console.ReadLine();
+int Number = Convert.ToInt32(A);
+
+if (Number <= 1) 
+{
+    Console.WriteLine("There's nothing to show");
+}
+
+else 
+{
+    while (N <= Number)
+    {
+        Console.Write($"{N} ");
+        N = N + 2;
+    }
+}
